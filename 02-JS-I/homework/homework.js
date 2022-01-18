40f86096627e61a8cc26597fcb8b4ad2e9ba64bc
ggl-can <58226338+ggl-can@users.noreply.github.com>
2022-01-17 18:59:02 -0600

    17 ene 2022

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -2,7 +2,7 @@
 
 // Crea una variable "string", puede contener lo que quieras:
 
-const nuevaString = "hola";
+const nuevaString = "HOLA";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
