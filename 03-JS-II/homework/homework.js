b1a10e72b6a915eda3f4f234171044ecfae3f7a5
ggl-can <58226338+ggl-can@users.noreply.github.com>
2022-01-18 14:11:29 -0600

    18ENE2022

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -17,8 +17,8 @@ function mayoriaDeEdad(edad) {
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
 
-if (edad >== 18 ){return "Allowed";}
-  else {return "Not Allowed";}
+if ( edad >= 18 ){ return "Allowed";}
+  else { return "Not allowed";}
 
 }
   
