6c2dafa9c205eb8a14a3f0d67d883fb28b326584
ggl-can <58226338+ggl-can@users.noreply.github.com>
2022-01-21 16:07:13 -0600

    21/ene/2022

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,33 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
-}
-
+    
+  var n = nombre.split(""); 
+  n[0]= n[0].toUpperCase();  
+  return n.reduce(function(cadF,letra){return cadF+letra;});
+   
+  }
+  
+  
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  //function area(base,altura){
+    //   return base*altura;
+  //}
+
+  //function dameArea(b,a,cb){
+       return cb;
+  //}
+  
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+
+  return cb(n1,n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +38,17 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  
+  cb(numeros)
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+
+
+  array.forEach(cb);
 }
 
 function map(array, cb) {
@@ -35,12 +56,22 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var nuevoArreglo = array.map(cb);
+  return nuevoArreglo;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  var sinA= [];
+
+for (var i=0; i< array.length ; i++){
+  if (array[i].charAt(0) == "a") { sinA.push(array[i]) }
+}
+
+return sinA; 
+
 }
 
 // No modificar nada debajo de esta línea
